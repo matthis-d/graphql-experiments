01643dd7fa18529f726179fc531ded940bb8e09e
Matthis Duclos <matthis.duclos@ratp.fr>
2017-06-22 13:54:25 +0200

    Implement mutations

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const crypto = require('crypto');
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
+
+const fakeDataBase = {};
 
 const schema = buildSchema(`
   type RandomDie {
@@ -9,12 +12,37 @@ const schema = buildSchema(`
     roll(numRolls: Int!): [Int]
   }
   
+  input MessageInput {
+    content: String
+    author: String
+  }
+  
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+  
   type Query {
     quoteOfTheDay: String
     random: Float!
     getDie(numSides: Int): RandomDie
+    getMessage(id: ID!): Message
+  }
+  
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
   }
 `);
+
+class Message {
+  constructor(id, { content, author }) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
 
 class RandomDie {
   constructor(numSides = 6) {
@@ -38,6 +66,24 @@ const root = {
   quoteOfTheDay: () => (Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'),
   random: () => Math.random(),
   getDie: ({ numSides }) => new RandomDie(numSides),
+  getMessage: ({ id }) => {
+    if (!fakeDataBase[id]) {
+      throw new Error(`no message exists with id ${id}`);
+    }
+    return new Message(id, fakeDataBase[id]);
+  },
+  createMessage: ({ input }) => {
+    const id = crypto.randomBytes(10).toString('hex');
+    fakeDataBase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: (id, { input }) => {
+    if (!fakeDataBase[id]) {
+      throw new Error(`no message exists with id ${id}`);
+    }
+    fakeDataBase[id] = input;
+    return new Message(id, input);
+  },
 };
 
 const app = express();
